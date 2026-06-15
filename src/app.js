f51de6e8f0ef685bb60ab46aab4d8ec0168f2a39
bikashroy120 <bikashroydt@gmail.com>
2026-06-15 09:18:38 +0600

    cors error fixed

--- src/app.js
+++ src/app.js
@@ -47,6 +47,7 @@ app.use(
             'http://localhost:5174',
             'http://localhost:3000',
             'http://localhost:3001',
+            'https://api-simonemiotto.maktechgroup.tech',
           ],
     credentials: true,
   }),
