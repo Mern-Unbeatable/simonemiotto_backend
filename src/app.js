2b8393a945735ee0d2eb97f377c60aaba5ba6d0c
bikashroy120 <bikashroydt@gmail.com>
2026-06-15 09:25:11 +0600

    add

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use(
             'http://localhost:5174',
             'http://localhost:3000',
             'http://localhost:3001',
-            'hhttps://simonemiotto.maktechgroup.tech',
+            'https://simonemiotto.maktechgroup.tech',
           ],
     credentials: true,
   }),
