dd1aaa8782dd8ee994ca01158b05e23f3c0363d5
bikashroy120 <bikashroydt@gmail.com>
2026-06-25 16:01:22 +0600

    email problem fixed

--- src/utils/email.js
+++ src/utils/email.js
@@ -5,23 +5,23 @@ const config = require('../config');
 
 class EmailService {
   constructor() {
-    // this.transporter = nodemailer.createTransport({
-    //   service: 'gmail',
-    //   auth: {
-    //     user: 'mdrakibulhasan12346@gmail.com',
-    //     pass: 'rbyz nvgi eppd rwqm',
-    //   },
-    // });
-
     this.transporter = nodemailer.createTransport({
-      host: config.mailchimp.smtp.host,
-      port: process.env.SMTP_PORT,
-      secure: false,
+      service: 'gmail',
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        user: 'mdrakibulhasan12346@gmail.com',
+        pass: 'rbyz nvgi eppd rwqm',
       },
     });
+
+    // this.transporter = nodemailer.createTransport({
+    //   host: config.mailchimp.smtp.host,
+    //   port: process.env.SMTP_PORT,
+    //   secure: false,
+    //   auth: {
+    //     user: process.env.SMTP_USER,
+    //     pass: process.env.SMTP_PASS,
+    //   },
+    // });
   }
 
   async sendMail(to, subject, text, html) {
